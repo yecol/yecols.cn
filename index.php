93b1a0f1eabecac7b43ec93b50d57df217c1c33f
yecol <xujb1987@gmail.com>
2013-09-22 20:39:45 +0800

    add template engine H2o to generate static htmls.

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+	require 'inc/h2o.php';
+	
+	$time_start = microtime(true);
+
+	h2o::load('cache'); 
+
+  	$tpl = new h2o('inc/tpls/portfolio.html', array(
+  		'cache'=>'file',
+  		'cache_dir'=>'cache',
+  		'cache_ttl' => 3600
+  		));
+
+    echo $tpl->render(array(
+    	'title'=>'Yecols.cn'
+    	));
+
+	echo "<!--Generate on ".date("Y-m-d H:i:s").", in ".(microtime(true) - $time_start)." seconds, with memory size ".memory_get_usage()." -->";
+?>
+
