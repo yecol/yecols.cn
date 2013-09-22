61fff18b9d0254f38761d5c0be42ab38af9e1ebe
yecol <xujb1987@gmail.com>
2013-09-22 11:15:16 +0800

    clear version1

--- blog/wp-content/themes/fusion/sidebar2.php
+++
@@ -1,9 +0,0 @@
- <!-- 2nd sidebar -->
-<div id="sidebar2">
-  <ul>
-    <?php
-      if (function_exists('dynamic_sidebar') && dynamic_sidebar(2) ) : else : ?>
-       <li>hey look another sidebar :)<br />Add some widgets here to make me dissapear</li>
-      <?php endif; ?>
-  </ul>
-</div><!-- /2nd sidebar -->
