c34b968137dca7dabd6c6af1311fb0ac93df139b
Jingbo <xujb1987@gmail.com>
2016-07-12 22:05:54 +0100

    Update generate.py

--- inc/generate.py
+++ inc/generate.py
@@ -502,11 +502,6 @@ def generateFiles():
 
 	shutil.copy2(root_dir+'inc/index.html', root_dir+'index.html')
 
-	try:
-		os.remove(root_dir+'index.html')
-	except OSError:
-		pass
-
 	#generate files
 	processSinglePage("about");
 	processSinglePage("portfolio");
